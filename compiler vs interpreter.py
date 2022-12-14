69032dfe89d7b392cc819a6d3407a66741ed479a
KavaliSrikanth <srikanthnaidu035@gmail.com>
2022-12-14 17:48:27 +0530

    assenment 1

---
+++ compiler vs interpreter.py
@@ -0,0 +1,5 @@
+Compiler                            interpreter
+1.one time Compiler                 1.line by line
+2.produce intermediate              2.donot produce intermediate
+code                                  code
+3.ex:c,c++,java                     3.ex:python,ruby
