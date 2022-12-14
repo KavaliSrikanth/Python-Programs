b068044d3857d59baea0298a14275f666c834a45
KavaliSrikanth <srikanthnaidu035@gmail.com>
2022-12-14 17:52:48 +0530

    assenment 1

---
+++ list vs set.py
@@ -0,0 +1,5 @@
+list                                          set
+1.ordered                                     1.unordered
+2.indexed                                     2.no indexes
+3.allow dupicates                             3.no duplicates are allowed
+4.[]                                          4.{}
