78ee0cc7cd6a3452e6a37356a4f5f9521b3cc6a3
KavaliSrikanth <srikanthnaidu035@gmail.com>
2022-12-14 17:17:11 +0530

    list vs tuple

---
+++ list vs tuple.py
@@ -0,0 +1,4 @@
+list                                          tuple
+1.mutable                                     1.immutable
+2.can be manupulated                          2.not manupulated
+3.[]                                          3.()
